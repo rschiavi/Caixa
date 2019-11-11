46261ac8c571d4908fd5e49629d33b08d455cd9b
Rafael <mrc_rafa@hotmail.com>
2019-11-11 11:41:11 -0300

    database

---
+++ database/migrations/2019_11_11_140704_create_movements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMovementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movements', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id');
+            $table->enum('type', ['R', 'D']);
+            $table->text('description');
+            $table->timestamp('date');
+            $table->unsignedInteger('value');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movements');
+    }
+}
